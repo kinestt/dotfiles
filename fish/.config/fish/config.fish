1c310709f6464863419ee86820778a361dee3b94
kin <kin@debian.lan>
2026-05-02 04:06:59 +0530

    commit

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,4 +7,13 @@ if test "$TERM" = "xterm-kitty"
     alias ssh="kitty +kitten ssh"
 end
 
-fish_add_path /home/kin/.spicetify
+fish_add_path /home/kin/.spicetify /home/kin/git-repos/yazi/target/release
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
